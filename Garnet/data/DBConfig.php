7b663c41e7217f269c1eed55ca5db0412db72ff9
Gregory Hermans <Hermgreg00@gmail.com>
2024-06-13 21:41:01 +0200

    improved garnet

--- Garnet/data/DBConfig.php
+++ Garnet/data/DBConfig.php
@@ -1,20 +1,6 @@
 <?php
 
 
-// class DBConfig
-// {
-//     public static $DB_CONNSTRING = "mysql:host=grego1-garnet.db.transip.me;dbname=grego1_garnet;charset=utf8";
-//     public static $DB_USERNAME = "grego1_garnetAdmin";
-//     public static $DB_PASSWORD = "5454564dsd897SD98DSDHSD87087777!è!çèç!è";
-
-//     public static function getDatabaseName()
-//     {
-//         $dbName = explode(';', self::$DB_CONNSTRING);
-//         $dbName = explode('=', $dbName[1]);
-//         return $dbName[1];
-//     }
-// }
-
 
 class DBConfig
 {
